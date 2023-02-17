9207b0a963ed52d796473c9e42e8e7e5322b192a
Gian Niehues <gian.niehues@edifyeducation.com.br>
2023-02-17 16:04:33 -0300

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'http://localhost:3000'
+
+const getHealth = async () => {
+    const resp = await fetch(`${BASE_URL}/health`)
+
+    return await resp.json()
+}
+
+
+const sendAction = async (action) => {
+
+    const resp = await fetch(`${BASE_URL}/sendAction`, {
+        method: "POST",
+        body: action,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+
+    return await resp.json()
+
+}
